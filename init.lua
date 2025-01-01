7d82235c08aa56373b6cb91a1eba6fa6c65d1c48
Moisés Vilas Boas <moisesboas@gmail.com>
2025-01-01 15:29:49 -0300

    feat: initial stuffs

---
+++ init.lua
@@ -0,0 +1,51 @@
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+vim.g.mapleader = " "
+
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
+end
+vim.opt.rtp:prepend(lazypath)
+
+local plugins = {
+  { "Mofiqul/dracula.nvim" },
+  {
+    'nvim-telescope/telescope.nvim', tag = '0.1.8',
+      dependencies = { 'nvim-lua/plenary.nvim' }
+  },
+  {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"}
+}
+
+local opts = {}
+
+require("lazy").setup(plugins, opts)
+
+local builtin = require("telescope.builtin")
+vim.keymap.set('n', '<C-p>', builtin.find_files,{}) -- CTRL + P = show telescope
+vim.keymap.set('n', '<leader>ft', builtin.live_grep, {}) -- SPACE + fg = find text
+
+local config = require("nvim-treesitter.configs")
+config.setup({
+  ensure_installed = {"lua", "javascript", "typescript", "go", "html", "json", "css", "tsx"},
+  highlight = {enable = true},
+  indent = { enable = true},
+})
+
+vim.cmd[[colorscheme dracula]]
+
+
+
